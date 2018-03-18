0f4f46cb70a0a358307c0e31e19e25f1a2a1fcc5
Hraph <lepointgeek@gmail.com>
2018-03-18 21:16:41 +0100

    Export DisplayColor enum to avoid loop include (between Frame / IDisplay)

---
+++ lib/ePDisplays/DisplayColor.hpp
@@ -0,0 +1,15 @@
+#ifndef DISPLAYCOLOR_H
+#define DISPLAYCOLOR_H
+
+#pragma once
+
+
+namespace Displays {
+    enum DisplayColor {
+        Uncolored = 0,
+        Colored
+    };
+
+}
+
+#endif /* DISPLAYCOLOR_H */
--- lib/ePDisplays/IDisplay.hpp
+++ lib/ePDisplays/IDisplay.hpp
@@ -1,21 +1,19 @@
 #ifndef IDISPLAY_H
 #define IDISPLAY_H
 #include "DisplayConnector.hpp"
+#include "DisplayColor.hpp"
 #include <Frame.hpp>
 
 #pragma once
 
-namespace Displays {
-    enum DisplayColor {
-        Uncolored = 0,
-        Colored
-    };
+using namespace UI;
 
+namespace Displays {
     class IDisplay : public DisplayConnector {
     public:
         virtual int  Init(void) = 0;
-        virtual void SetFrame(UI::Frame *frame, DisplayColor color) = 0;
-        virtual void SetDualFrames(UI::Frame *uncoloredFrame, UI::Frame *coloredFrame) = 0;
+        virtual void SetFrame(Frame *frame, DisplayColor color) = 0;
+        virtual void SetDualFrames(Frame *uncoloredFrame, Frame *coloredFrame) = 0;
         virtual void DisplayFrame(void) = 0;
         virtual void ClearFrame(void) = 0;
         virtual void Sleep(void) = 0;
