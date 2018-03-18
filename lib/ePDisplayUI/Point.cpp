265f9fca10294e152a74dbf628254328a31e4acd
Hraph <lepointgeek@gmail.com>
2018-03-19 00:53:43 +0100

    Redo default Color to Point

--- lib/ePDisplayUI/Point.cpp
+++ lib/ePDisplayUI/Point.cpp
@@ -4,6 +4,10 @@
 using namespace Displays;
 
 namespace UI {
+    Point::Point(int x, int y){
+        this->x = x;
+        this->y = y;
+    };
     Point::Point(int x, int y, DisplayColor color){
         this->x = x;
         this->y = y;
--- lib/ePDisplayUI/Point.hpp
+++ lib/ePDisplayUI/Point.hpp
@@ -14,6 +14,7 @@ namespace UI {
     class Point : public IUIElement {
         public:
             Point();
+            Point(int x, int y);
             Point(int x, int y, DisplayColor color);
             int GetX(void);
             int GetY(void);
@@ -24,7 +25,7 @@ namespace UI {
         private:
             int x;
             int y;
-            DisplayColor color;
+            DisplayColor color = DisplayColor::Uncolored;
         protected:
             Frame* Render(Frame *frame) = 0;
     };
