41a308a50340ac2c202918b7d1b090aefff40a9c
Hraph <lepointgeek@gmail.com>
2018-03-19 23:07:48 +0100

    Add control interface

---
+++ lib/ePDisplayUI/IControl.hpp
@@ -0,0 +1,15 @@
+#ifndef ICONTROL_H
+#define ICONTROL_H
+
+#pragma once
+
+#include "IUIElement.hpp"
+
+namespace UI {
+    class IControl : public IUIElement {
+
+    
+    };
+}
+
+#endif /* ICONTROL_H */
